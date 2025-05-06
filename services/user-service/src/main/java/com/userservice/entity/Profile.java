11bfdf94178c226b5e2e4baeeed22a8bbacfb234
Maksim Ostroginskiy <ostroginskiymaksim@gmail.com>
2025-05-06 21:16:26 +0300

    Liquid base migration for initial config user-service-db was craeted and corresponding entities were implemented

--- services/user-service/src/main/java/com/userservice/entity/Profile.java
+++ services/user-service/src/main/java/com/userservice/entity/Profile.java
@@ -1,4 +1,48 @@
 package com.userservice.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "profiles")
+@Getter
+@Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "profiles_seq")
+    private Long id;
+
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    private User user;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name = "phone_number", nullable = false, length = 30)
+    private String phoneNumber;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "avatar")
+    private String avatar;
+
+    public Profile(User user, String firstName, String lastName, String phoneNumber, LocalDate birthDate) {
+        this.user = user;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.avatar = null;
+    }
 }
--- services/user-service/src/main/java/com/userservice/entity/User.java
+++ services/user-service/src/main/java/com/userservice/entity/User.java
@@ -1,4 +1,64 @@
 package com.userservice.entity;
 
+import com.userservice.enums.AuthProviderEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String email;
+
+    private String password;
+
+    @Column(name = "is_blocked", nullable = false)
+    private boolean isBlocked = false;
+
+    @Column(name = "is_verified", nullable = false)
+    private boolean isVerified = false;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "auth_provider", nullable = false)
+    private AuthProviderEnum authProvider;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    public User(String email, String password, Set<Role> roles) {
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+        authProvider = AuthProviderEnum.LOCAL;
+    }
+
+    public User(String email, AuthProviderEnum providerEnum, Set<Role> roles) {
+        this.email = email;
+        this.roles = roles;
+        authProvider = providerEnum;
+    }
 }
