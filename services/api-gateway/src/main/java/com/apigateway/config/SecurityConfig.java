33419492e82ad8475743aa266d6b187aeffb4c3f
Maksim Ostroginskiy <ostroginskiymaksim@gmail.com>
2026-01-28 18:04:14 +0200

    fix: fix grpc channels env var management, fix docker compose

--- services/api-gateway/src/main/java/com/apigateway/config/SecurityConfig.java
+++ services/api-gateway/src/main/java/com/apigateway/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
             "/api/v1/categories/**",
             "/api/v1/brands/**",
             "/api/v1/products/**",
-
+            "/actuator/**"
     };
 
     private final JwtProperties jwtProperties;
